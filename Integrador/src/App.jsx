fd3010fc9df7feb37e41e58bf0d43062a6f76271
Felix <felixjgca@gmail.com>
2023-08-06 16:08:49 -0300

    integrador&react

---
+++ Integrador/src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar/Navbar";
+import Layout from "./components/Layout/Layout";
+import Hero from "./components/Hero/Hero";
+import Beneficios from "./components/Beneficios/Beneficios";
+import Categorias from "./components/Categorias/Categorias";
+import CardsProductos from "./components/Productos/CardsProductos";
+import Contacto from "./components/Contacto/Contacto";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Layout>
+        <Hero />
+        <Beneficios />
+        <Categorias />
+        <CardsProductos />
+        <Contacto />
+      </Layout>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
